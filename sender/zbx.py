085740a1a94e3f48791c2acbffbcb67a0d44405e
hot1232 <hot1232@163.com>
2017-03-20 16:34:07 +0800

    fix some bug

--- sender/zbx.py
+++ sender/zbx.py
@@ -11,16 +11,13 @@ class Sender(object):
         self._name="zbx"
         self.data=protobix.DataContainer(mType, "172.16.5.220", 10051)
         self.hostname="hello"
-    
-    def send(self,data):
+    def add(self,data):
+        self.data.add({self.hostname:data})
+    def send(self):
         try:
-            print("zbx send data: %s"%data)
-            data={self.hostname:data}
-            self.data.add(data)
             ret = self.data.send(self.data)
             if not ret:
-                print "Ooops. Something went wrong when sending data to Zabbix"    
-            print "Everything is OK"
+                print "Ooops. Something went wrong when sending data to Zabbix"
         except Exception,e:
             import traceback
             traceback.print_exc()
